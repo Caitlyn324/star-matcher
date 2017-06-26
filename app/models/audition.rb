cd0e00747ff1e858a4c0d4374da503e3d1e236e2
ChrisDonhue0628 <chris.donohue0628@gmail.com>
2017-06-26 17:01:30 -0400

    Codeship Test Change Duration

--- app/models/audition.rb
+++ app/models/audition.rb
@@ -1,2 +1,6 @@
 class Audition < ApplicationRecord
+  validates :roles, presence: true
+  validates :theater, presence: true
+  validates :address, presence: true
+  validates :company, presence: true
 end
