d9cd65ec738022af0ec4282868061ed8e2efe100
ChrisDonhue0628 <chris.donohue0628@gmail.com>
2017-07-06 11:13:21 -0400

    sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,7 @@ ActionMailer::Base.smtp_settings = {
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.default_url_options = { :host => 'star-matcher.heroku.com' }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
